a29176e7f3c618b3a5363e0c58b99897012dd738
christian vega jonguitud <chrisvega@live.com.mx>
2022-08-30 01:08:30 -0500

    initial commit

---
+++ HelloWorld.java
@@ -0,0 +1,12 @@
+
+
+class HelloWorld {
+	public static void main(String []ppp) {
+
+		if(ppp.length < 1){
+			System.out.println("Manda un parametro al menos");
+		}
+		System.out.println("Hello World!"+ppp[0]);
+
+	}
+}
